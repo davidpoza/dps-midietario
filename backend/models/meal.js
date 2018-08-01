6874e9c066ad169eb2341ae1d479283b9b8c9baf
David Poza Suarez <pozasuarez@gmail.com>
2018-08-01 10:51:09 +0200

    Meal schema

---
+++ backend/models/meal.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var MealSchema = Schema({
+    name: String,
+    hour: String,
+    foods: [{ type: Schema.ObjectId, ref:'Food' }]
+});
+
+module.exports = mongoose.model('Meal', MealSchema);

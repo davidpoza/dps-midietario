a588e5f9d77b1a789539d48ac9e459b68fae9aff
David Poza Suarez <pozasuarez@gmail.com>
2018-08-09 14:36:32 +0200

    boton guardar cambios en perfil. También subida de imagen

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../services/user.service';
+import { Global } from '../../../services/global';
 
+import { MatSnackBar } from '@angular/material';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  public url:string;
+  public token;
+  public identity;
+  public filesToUpload: Array<File>;
+  constructor(
+    private _userService: UserService,
+    public snackBar: MatSnackBar,
+    private _router: Router,
+  ) {
+    this.url = Global.url;
+    this.token = this._userService.getToken();
+    this.identity = this._userService.getIdentity();
+  }
 
   ngOnInit() {
   }
 
+  onSave(form){
+    this._userService.updateUser(this.identity._id, this.identity, this.token).subscribe(
+      response => {
+        if(this.filesToUpload) {
+          this._userService.makeFileRequest(this.url+"uploaduserimage/"+ response.user._id, [], this.filesToUpload, "image", this.token).then((result:any) => {
+            
+          });
+        }
+        this.snackBar.open("Cambios guardados.", '', {
+          duration: 500,
+        });
+        //cargamos los datos nuevos en la sesión
+        localStorage.setItem("identity", JSON.stringify(response.user));
+      },
+      error => {
+        this.snackBar.open("Error al guardar cambios.", '', {
+          duration: 500,
+        });
+      }
+    );
+  }
+  
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+  }
 }
--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -68,4 +68,43 @@ export class UserService{
         .set('Authorization', token);
         return this._http.get(this.url+'usersbyemail/'+email, {headers:headers});
     }
+
+    updateUser(userId, update, token): Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/json')
+        .set('Authorization', token);
+        let params = JSON.stringify(update);
+        return this._http.put(this.url+'users/'+userId, params, {headers:headers});
+    }
+
+    /* Creamos un objeto ajax contra la url de subida de ficheros
+     y un objeto formulario donde añadimos todos los ficheros.
+     
+     Llamaremos a esta url del api justo despues de haber añadido un item
+     con exito.*/
+     makeFileRequest(url: string, params: Array<string>, files: Array<File>, name: string, token){
+        return new Promise(function(resolve, reject){
+            var formData: any = new FormData();
+            var xhr = new XMLHttpRequest();
+
+            for(var i = 0; i < files.length; i++){
+                formData.append(name, files[i], files[i].name);
+            }
+
+            xhr.onreadystatechange = function(){
+                if(xhr.readyState == 4){
+                    if(xhr.status == 200){
+                        resolve(JSON.parse(xhr.response));
+                    }
+                    else{
+                        reject(xhr.response);
+                    }
+                }
+            }
+
+            xhr.open("POST", url, true);
+            //añadimos la cabecera de autorizacion
+            xhr.setRequestHeader('Authorization',token);
+            xhr.send(formData);
+        });
+    }
 }
